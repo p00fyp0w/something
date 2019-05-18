355929a6cbe61bac32cc3ce30d4e47e3b3ca3541
Poofy <rahulpaul@rahuls-air.wifi.int.ryerson.ca>
2019-05-18 16:56:41 -0400

    initial

---
+++ java/com/example/idk/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.idk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNextPage();
+            }
+        });
+
+    }
+
+    public void openNextPage(){
+        Intent intent = new Intent(this, AfterLogin.class);
+        startActivity(intent);
+
+    }
+}
